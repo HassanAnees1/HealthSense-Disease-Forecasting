dc5c5b6491551172288f0aaa899d0e827c536a0a
Hassan Elzeny <107946473+HassanAnees1@users.noreply.github.com>
2025-05-10 17:50:10 +0300

    Delete src directory

--- src/train_rl.py
+++
@@ -1,8 +0,0 @@
-import gym
-from stable_baselines3 import DQN
-
-# بيئة تجريبية (CartPole مثال)
-env = gym.make('CartPole-v1')
-model = DQN('MlpPolicy', env, verbose=1)
-model.learn(total_timesteps=10000)
-model.save('models/dqn_cartpole')
